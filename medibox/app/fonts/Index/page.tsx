3c74ea659f1d42b687b326edacb7c9da466e5aa5
BuddhiniNavoda <buddhinikaluwila1999@gmail.com>
2025-03-06 23:15:44 +0530

    eddit file to goto signup page

--- medibox/app/fonts/Index/page.tsx
+++ medibox/app/fonts/Index/page.tsx
@@ -13,9 +13,11 @@ const IndexPage = () => {
       <h1 className="text-6xl font-bold mb-8">MedReminder</h1>
 
       <div className="flex space-x-4">
+      <Link href="/fonts/SignUp">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Sign Up
         </button>
+        </Link>
 
         <Link href="/fonts/Login">
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
