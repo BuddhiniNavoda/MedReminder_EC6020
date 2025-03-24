b2a66a6e868530c56c1ca7769395537924a78745
BuddhiniNavoda <buddhinikaluwila1999@gmail.com>
2025-03-24 20:18:01 +0530

    change the link

--- medibox/app/Index/page.tsx
+++ medibox/app/Index/page.tsx
@@ -13,13 +13,13 @@ const IndexPage = () => {
       <h1 className="text-6xl font-bold mb-8">MedReminder</h1>
 
       <div className="flex space-x-4">
-      <Link href="/fonts/SignUp">
+      <Link href="/SignUp">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Sign Up
         </button>
         </Link>
 
-        <Link href="/fonts/Login">
+        <Link href="/Login">
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             Sign In
           </button>
