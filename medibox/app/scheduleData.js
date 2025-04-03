4acc8b41e5b28cb31b5aacfacbd6abf607008cb3
BuddhiniNavoda <buddhinikaluwila1999@gmail.com>
2025-04-03 15:58:26 +0530

    make a hard code data forr test get method

---
+++ medibox/app/scheduleData.js
@@ -0,0 +1,7 @@
+export const mockSchedule = [
+    { id: "1", date: "2025-04-01", time: "08:00", boxType: "A" },
+    { id: "2", date: "2025-04-02", time: "12:00", boxType: "B" },
+    { id: "3", date: "2025-04-03", time: "18:30", boxType: "A" },
+    
+  ];
+  
