4c194a62bacb4dad0829d9d6c81aeacfbfde43fb
BuddhiniNavoda <buddhinikaluwila1999@gmail.com>
2025-04-07 20:06:10 +0530

    rename files

--- medibox/app/page.tsx
+++ medibox/app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
       <h1 className="text-6xl font-bold mb-8">MedReminder</h1>
 
       <div className="flex space-x-4">
-      <Link href="/signup">
+      <Link href="/sign_up">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Sign Up
         </button>
         </Link>
 
-        <Link href="/login">
+        <Link href="/login_page">
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             Sign In
           </button>
